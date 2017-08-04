6f3ae9a3a164534344bbce92f840d102986b3517
thomaslam <tuananhlam95@gmail.com>
2017-08-04 16:07:57 -0400

    change marker, overlay

--- energy_map/static/js/main.js
+++ energy_map/static/js/main.js
@@ -1,36 +1,84 @@
+function getLayer(type) {
+  return L.gridLayer.googleMutant({
+    'type': type, // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
+    attribution: '&copy; 2017 AC Office of Environmental Sustainability'
+  })
+}
+
+function initChart(canvasID, data) {
+  return new Chart(canvasID, {
+          type: 'line',
+          data: {
+            labels: data['time_data'],
+            datasets: [{
+              data: data['energy_data'],
+              fill: false,
+              backgroundColor: '#614488',
+              borderColor: '#614488',
+              borderWidth: 3,
+              pointStyle: 'circle',
+              tension: 0.25
+            }]
+          },
+          options: {
+            responsive: true,
+            legend: {
+              display: false
+            },
+            tooltips: {
+              mode: 'index',
+              intersect: false,
+            },
+            hover: {
+              mode: 'nearest',
+              intersect: true
+            },
+            scales: {
+              xAxes: [{
+                gridLines: {
+                  display: false
+                },
+                display: true,
+                scaleLabel: {
+                  display: true,
+                  labelString: 'Time'
+                },
+                ticks: {
+                  callback: function(value, index, values) {
+                    return '';
+                  }
+                }
+              }],
+              yAxes: [{
+                display: true,
+                scaleLabel: {
+                  display: true,
+                  labelString: 'kWh'
+                }
+              }]
+            }
+          }
+        });
+}
+
+var roads = getLayer('roadmap'),
+    satellite = getLayer('satellite'),
+    terrain = getLayer('terrain'),
+    hybrid = getLayer('hybrid');
+
+var mymap = L.map('mapid', {
+  layers: [satellite, terrain, hybrid, roads]
+}).setView([42.3709104, -72.5170028], 16);
+
+L.control.layers({
+  "Roads": roads,
+  "Satellite": satellite,
+  "Terrain": terrain,
+  "Hybrid": hybrid
+}).addTo(mymap);
+
 main = {
-    init: function(housingData) {
-      var roads = L.gridLayer.googleMutant({
-        type: 'roadmap', // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
-        attribution: '&copy; 2017 AC Office of Environmental Sustainability'
-      });
-
-      var satellite = L.gridLayer.googleMutant({
-        type: 'satellite', // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
-        attribution: '&copy; 2017 AC Office of Environmental Sustainability'
-      });
-
-      var terrain = L.gridLayer.googleMutant({
-        type: 'terrain', // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
-        attribution: '&copy; 2017 AC Office of Environmental Sustainability'
-      });
-
-      var hybrid = L.gridLayer.googleMutant({
-        type: 'hybrid', // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
-        attribution: '&copy; 2017 AC Office of Environmental Sustainability'
-      });
-
-      var mymap = L.map('mapid', {
-        layers: [satellite, terrain, hybrid, roads]
-      }).setView([42.3709104, -72.5170028], 16);
-
-      L.control.layers({
-        "Roads": roads,
-        "Satellite": satellite,
-        "Terrain": terrain,
-        "Hybrid": hybrid
-      }).addTo(mymap);
-
+    init: function(housingData, staticFolder) {
       function getColor(d) {
         return d > 300 ? '#7a0177' :
                d > 200 ? '#ae017e' :
@@ -43,28 +91,22 @@ main = {
 
       function style(feature) {
         return {
-            // fillColor: getColor(feature.properties.electricity),
             weight: 0,
             opacity: 0,
-            color: 'white',
-            // dashArray: '3'
-            // fillOpacity: 1
+            color: 'white'
         };
       }
 
+      // https://ceed.ucdavis.edu/modules/campusHome/img/map/classroom-marker.svg
       var customIcon = L.icon({
-        iconUrl: 'https://ceed.ucdavis.edu/modules/campusHome/img/map/classroom-marker.svg',
+        iconUrl: staticFolder + "img/marker.svg",
         iconAnchor: [7, 7],
         iconSize: [15, 15]
       });
 
       function onEachFeature(feature, layer) {
         if (feature.properties && feature.properties.marker) {
-          var marker = L.marker(feature.properties.marker, {icon: customIcon});
-
-          marker.bindPopup("<b>" + feature.properties.name + "</b><br>Previous day total: " 
-            + feature.properties.electricity + " kWh<br><a href='/energy_data?bld=' class='.popupLink'>View energy usage by hour</a>").
-            addTo(mymap);
+          
 
           L.circle(feature.properties.marker, {
             radius: 50,
@@ -76,97 +118,63 @@ main = {
         }
       }
 
+      function pointToLayer(feature, latlng) {
+        var marker = L.marker(latlng, {icon: customIcon});
+
+        marker.bindPopup("<b>" + feature.properties.name + "</b><br>Previous day total: " 
+          + feature.properties.electricity + " kWh<br><a href='#'>View energy usage by hour</a>").
+              on('click', function(e) {
+                mymap.setView(e.target.getLatLng(), 16);
+              });
+
+        return marker;
+      }
+
       L.geoJson(housingData, {
-        style: style,
-        onEachFeature: onEachFeature
+        // style: style,
+        onEachFeature: onEachFeature,
+        pointToLayer: pointToLayer
       }).addTo(mymap);
 
       mymap.on('popupopen', function(e) {
-        console.log("popupopen event");
-
         var marker = e.popup._source;
 
         var aLink = marker._popup._contentNode.childNodes[4];
-        console.log(aLink);
-
         var bldName = marker._popup._contentNode.childNodes[0].textContent;
-        console.log(bldName);
+        console.log("Clicked on bldName: " + bldName);
 
         aLink.onclick = function(e) {
           e.preventDefault();
 
           console.log('Clicked inside popup');
 
+          // Show overlay element 
           $.ajax({url: '/energy_data?bld=' + bldName, success: function(data, status) {
             var overlay = $('#overlay');
             overlay.show();
 
             $('#overlay-text').text(bldName+ " energy usage for past 24 hours")
-            
-            console.log(bldName);
+
             console.log(data['energy_data']);
 
-            var myChart = new Chart($('#myChart'), {
-              type: 'line',
-              data: {
-                labels: data['time_data'],
-                datasets: [{
-                  data: data['energy_data'],
-                  fill: false,
-                  backgroundColor: '#614488',
-                  borderColor: '#614488',
-                  borderWidth: 3,
-                  pointStyle: 'circle',
-                  tension: 0.25
-                }]
-              },
-              options: {
-                responsive: true,
-                legend: {
-                  display: false
-                },
-                tooltips: {
-                    mode: 'index',
-                    intersect: false,
-                },
-                hover: {
-                    mode: 'nearest',
-                    intersect: true
-                },
-                scales: {
-                    xAxes: [{
-                        gridLines: {
-                          display: false
-                        },
-                        display: true,
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Time'
-                        },
-                        ticks: {
-                          callback: function(value, index, values) {
-                            return '';
-                          }
-                        }
-                    }],
-                    yAxes: [{
-                        display: true,
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'kWh'
-                        }
-                    }]
-                }
-              }
-            });
+            var myChart = initChart($('#myChart'), data);
+
+            // Make background darker
+            $('#mapid').css('opacity', 0.5);
+            $('.main-panel').css('background-color', 'black');
 
             $('.close').click(function() {
               overlay.hide();
+
+              // Make background normal
+              $('#mapid').css('opacity', 1);
+              $('.main-panel').css('background-color', '');
+
               // Prevent hovering problem
               $('#myChart').remove();
               $('#overlay').append('<canvas id="myChart"></canvas>');
-            })
-          }})
+            });
+          }});
         };
       });
       
@@ -179,6 +187,7 @@ main = {
           labels = [];
 
         // loop through our density intervals and generate a label with a colored square for each interval
+        div.innerHTML = ''
         for (var i = 0; i < grades.length; i++) {
           div.innerHTML +=
           '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
